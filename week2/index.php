d069f75ab56e1f0384269a96b621d7c22ea22d9f
Alyssa <=>
2025-05-28 16:09:02 -0400

    push week 3

---
+++ week2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <title>Week 2</title>
+</head>
+<body>
+
+   <!-- Learning how to use echo in php -->
+
+   <?php
+
+
+     echo "<h1>Hello World </h1>";
+     $firstname = "Alyssa";
+     $lastname = "Austin";
+     $program = "Web Development";
+     echo "$lastname  $firstname  $program";
+     ?>
+    
+</body>
+</html>
